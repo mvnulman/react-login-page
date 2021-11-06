6307478dc615f0c09439d52f5b3af35cfdf7ed21
Marcos Vinicius <mvsouza7@gmail.com>
2021-11-06 21:16:39 +0200

    docs: main component structured

--- src/App.js
+++ src/App.js
@@ -1,36 +1,36 @@
-import './App.css';
-import styled from 'styled-components';
-import bgImg from '../src/assets/bg-img.png';
-import Sidebar from './components/Sidebar';
-import Main from './components/Main';
+import "./App.css";
+import styled from "styled-components";
+import bgImg from "../src/assets/bg-img.png";
+import Sidebar from "./components/Sidebar";
+import Main from "./components/Main";
 
 const App = () => {
   return (
-    <Container >
-      <Wrapper >
+    <Container>
+      <Wrapper>
         <Sidebar />
         <Main />
       </Wrapper>
     </Container>
   );
-}
+};
 
 const Container = styled.div`
-background-color: #EEFCFF;
-position: absolute;
-top: 0;
-left: 0;
-bottom: 0;
-right: 0;
-`
+  background: #eefcff;
+  position: absolute;
+  top: 0;
+  left: 0;
+  bottom: 0;
+  right: 0;
+`;
 
 const Wrapper = styled.div`
-background-image: url(${bgImg});
-background-position: center;
-background-size: cover;
-background-repeat: no-repeat;
-widht: 100%;
-height: 100%;
-
-`
+  background-image: url(${bgImg});
+  background-position: center;
+  background-size: cover;
+  background-repeat: no-repeat;
+  width: 100%;
+  height: 100%;
+  display: flex;
+`;
 export default App;
--- src/components/Main.js
+++ src/components/Main.js
@@ -1,11 +1,30 @@
-import React from 'react'
+import React from "react";
+import styled from "styled-components";
 
 const Main = () => {
-    return (
-        <div>
-            <h1>Main</h1>
-        </div>
-    )
-}
+  return (
+    <Container>
+      <h1>
+        Join The <br />
+        Team
+      </h1>
+    </Container>
+  );
+};
 
-export default Main
+const Container = styled.div`
+  width: 100%;
+  height: 100%;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  h1 {
+    font-size: 70px;
+    font-weight: 900;
+    @media (max-width: 900px) {
+        display:none;
+    }
+  }
+`;
+
+export default Main;
