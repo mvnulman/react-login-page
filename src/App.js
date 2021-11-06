02a56cfd5c3c2f8bb5412df1743cf2f0b54f1e4b
Marcos Vinicius <mvsouza7@gmail.com>
2021-11-06 20:48:24 +0200

    docs: added main.js and sidebar.js to the project

--- src/App.js
+++ src/App.js
@@ -1,11 +1,36 @@
 import './App.css';
+import styled from 'styled-components';
+import bgImg from '../src/assets/bg-img.png';
+import Sidebar from './components/Sidebar';
+import Main from './components/Main';
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <h1>Testing 1, 2, 3</h1>
-    </div>
+    <Container >
+      <Wrapper >
+        <Sidebar />
+        <Main />
+      </Wrapper>
+    </Container>
   );
 }
 
+const Container = styled.div`
+background-color: #EEFCFF;
+position: absolute;
+top: 0;
+left: 0;
+bottom: 0;
+right: 0;
+`
+
+const Wrapper = styled.div`
+background-image: url(${bgImg});
+background-position: center;
+background-size: cover;
+background-repeat: no-repeat;
+widht: 100%;
+height: 100%;
+
+`
 export default App;
