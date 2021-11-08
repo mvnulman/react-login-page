b3137f5ac97c98cf07b903a9c9dc1b8507041116
Marcos Vinicius <mvsouza7@gmail.com>
2021-11-08 18:21:01 +0200

    docs: added color validation with styled components to the form inputs

--- src/components/Input.js
+++ src/components/Input.js
@@ -4,8 +4,13 @@ import styled from "styled-components";
 const Input = ({ type, placeholder }) => {
   return (
     <Container>
-      <StyledInput type="text" />
-      <div />
+      <StyledInput
+        placeholder={placeholder}
+        type={type ? type : "text"}
+        required
+        autocomplete="on"
+      />
+      <Status />
     </Container>
   );
 };
@@ -13,14 +18,39 @@ const Input = ({ type, placeholder }) => {
 const StyledInput = styled.input`
   width: 80%;
   max-width: 350px;
-  min-width: 250px;
-  height: 40px;
+  min-width: 300px;
+  height: 50px;
   margin: 0.5rem 0;
   background-color: #f3f3f3;
   border: none;
   box-shadow: 0px 14px 9px -15px rgba(0, 0, 0, 0.25);
   border-radius: 10px;
   padding: 0 1rem;
+  transition: all 0.2s ease-in;
+
+  &:hover {
+    transform: translateY(-5px);
+  }
+`;
+
+const Status = styled.div`
+  height: 10px;
+  width: 10px;
+  background: #9d9d9d;
+  border-radius: 10px;
+  margin-left: 1rem;
+
+  ${StyledInput}:focus + & {
+    background: #ffa689;
+  }
+
+  ${StyledInput}:invalid + & {
+    background: #fe2f75;
+  }
+
+  ${StyledInput}:valid + & {
+    background: #70EDB9;
+  }
 `;
 
 const Container = styled.div`
